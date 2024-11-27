f152fbc192d25626fe575cea006f0649016b8813
Su Gi Chandran <chandran.su.gi@gmail.com>
2024-11-27 02:40:09 -0800

    Improve data_transformation

--- data_transformation.ts
+++ data_transformation.ts
@@ -1,4 +1,5 @@
-interface DatePlayData {
+// Output format
+interface PlaycountGraphData {
     x: string,
     y: number,
     tooltip: string
@@ -15,73 +16,90 @@ interface RadioStationData {
     tracks: TrackData[]
 }
 
+interface PlaycountData {
+    [trackName: string]: number
+}
+
+function isTrackData(input: any): input is TrackData {
+    return input && input.timestp && input.trackName &&
+    typeof(input.timestp) == 'string' && typeof(input.trackName) == 'string'; 
+}
+
 function isRadioStationData(input: any): input is RadioStationData {
     return input && input.id && input.name && input.tracks &&
-    typeof(input.id) == 'number' && typeof(input.name) == 'string';
+    typeof(input.id) == 'number' && typeof(input.name) == 'string' &&
+    input.tracks.every(isTrackData);
 }
 
-function tallyDailyPlaycount(playcountData: { [trackName: string] : number }): number {
-    let playcounts = 0;
+function tallyDailyPlaycount(playcountData: PlaycountData): number {
+    let playcount = 0;
 
     for (const trackName in playcountData) {
-        playcounts += playcountData[trackName];
+        playcount += playcountData[trackName];
     }
 
-    return playcounts;
+    return playcount;
 }
 
-function generateDailyTooltip(playcountData: { [trackName: string] : number }): string {
+function generateDailyTooltip(playcountData: PlaycountData): string {
     let tooltip = '';
+    let playcountArray = Object.entries(playcountData);
 
-    for (const trackName in playcountData) {
-        tooltip += trackName + '(' + String(playcountData[trackName]) + '),';
+    for (let i = 0; i < playcountArray.length; i++) {
+        if(i == playcountArray.length - 1) {
+            tooltip += playcountArray[i][0] + '(' + String(playcountArray[i][1]) + ')';
+        } else {
+            tooltip += playcountArray[i][0] + '(' + String(playcountArray[i][1]) + '), ';
+        }
     }
 
     return tooltip;
 }
 
-function generatePlaycountData(rawData: unknown): DatePlayData[] {
+function generateGraphData(rawData: unknown): PlaycountGraphData[] {
     if (!Array.isArray(rawData)) {
         throw new TypeError('Invalid radio station data');
     }
 
-    let dateDictionary: { [date: string]: { [trackName: string] : number } } = {};
-
+    let playcountsByDate: { [date: string]: PlaycountData } = {};
     for (const datapoint of rawData) {
         if (!isRadioStationData(datapoint)) {
             throw new TypeError('Invalid radio station data');
         }
 
         for (const track of datapoint.tracks) {
-            if (track.timestp in dateDictionary) {
-                if (track.trackName in dateDictionary[track.timestp]) {
-                    dateDictionary[track.timestp][track.trackName] += 1;
+            if (track.timestp in playcountsByDate) {
+                if (track.trackName in playcountsByDate[track.timestp]) {
+                    playcountsByDate[track.timestp][track.trackName] += 1;
                 } else {
-                    dateDictionary[track.timestp][track.trackName] = 1;
+                    playcountsByDate[track.timestp][track.trackName] = 1;
                 }
             } else {
-                dateDictionary[track.timestp] = { [track.trackName]: 1};
+                playcountsByDate[track.timestp] = { [track.trackName]: 1};
             }
         }
     }
 
-    let output: DatePlayData[] = [];
-
-    for (const date in dateDictionary) {
-        const datapoint: DatePlayData = {
+    let output: PlaycountGraphData[] = [];
+    for (const date in playcountsByDate) {
+        const datapoint: PlaycountGraphData = {
             x: date,
-            y: tallyDailyPlaycount(dateDictionary[date]),
-            tooltip: generateDailyTooltip(dateDictionary[date])
+            y: tallyDailyPlaycount(playcountsByDate[date]),
+            tooltip: generateDailyTooltip(playcountsByDate[date])
         };
 
         output.push(datapoint);
     }
 
+    output.sort((e1, e2) => {
+        return e1.x.localeCompare(e2.x);
+    });
+
     return output;
 }
 
 // Test cases
-console.log(JSON.stringify(generatePlaycountData([
+console.log(JSON.stringify(generateGraphData([
     {
       id: 1293487,
       name: "KCRW",  // radio station callsign
@@ -107,4 +125,8 @@ console.log(JSON.stringify(generatePlaycountData([
         { timestp: "2021-04-07", trackName: "Captain Hook" }
       ]
     }
-])));
+]), null, 2));
+
+console.log(generateGraphData([])); // Expected output: []
+
+console.log(generateGraphData({ "status": 400 })); // Expected output: throws TypeError
